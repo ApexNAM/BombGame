81fee89ff956bc593a8b8b4009741f0bb3273fe8
ApexNAM <46587415+ApexNAM@users.noreply.github.com>
2021-05-12 14:59:54 +0900

    05-12-21

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float LookX = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        transform.Translate(0, 0, z * 15f * Time.deltaTime);
+        transform.Rotate(Vector3.up * LookX * 100f * Time.deltaTime);
+    }
+}
