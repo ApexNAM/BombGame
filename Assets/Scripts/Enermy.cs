e39e4aee5edacbc92ab4210e69ec589e5affe100
ApexNAM <46587415+ApexNAM@users.noreply.github.com>
2021-05-26 14:16:08 +0900

    2021_05_26_2:15

---
+++ Assets/Scripts/Enermy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enermy : MonoBehaviour
+{
+    public Transform target;
+    private Rigidbody rb;
+    public float speed = 20f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if(Vector3.Distance(target.position,transform.position) < 10f)
+        {
+            Vector3 trans = target.position - transform.position;
+            rb.AddForce(trans * speed * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator animator;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,5 +19,29 @@ public class PlayerMovement : MonoBehaviour
 
         transform.Translate(0, 0, z * 15f * Time.deltaTime);
         transform.Rotate(Vector3.up * LookX * 100f * Time.deltaTime);
+    
+        if(LookX != 0 || z != 0)
+        {
+            animator.SetBool("walk", true);
+        }
+        else
+        {
+            animator.SetBool("walk", false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Obstacle")
+        {
+            animator.SetTrigger("damage");
+            GameManager.instance.AddScore(-1);
+        }
+
+        if(collision.collider.tag == "Enermy")
+        {
+            animator.SetTrigger("attack01");
+            GameManager.instance.OnPlayerDead();
+        }
     }
 }
